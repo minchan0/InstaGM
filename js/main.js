1a6430f792a1cd9dd6b9477078d04d725e5f8b2b
martin-yeo <108623304+martin-yeo@users.noreply.github.com>
2023-04-09 17:26:44 +0000

    cog 버튼 회전2

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 document.addEventListener("DOMContentLoaded", (e) => {
 
-    let icn = document.querySelector("fa-cog")
+    let icn = document.querySelector("#fa-cog")
     
     icn.addEventListener("mouseover", (e) => {
         e.target.setAttribute("class", "far fa-cog fa-spin");
+    });
+    icn.addEventListener("mouseout", (e) => {
+        e.target.setAttribute("class", "far fa-cog");
     });
 });
