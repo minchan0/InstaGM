6a206c842c3322dc899620b75c2ca3541a7cc51f
martin-yeo <108623304+martin-yeo@users.noreply.github.com>
2023-04-09 17:21:06 +0000

    main.js 추가

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", (e) => {
+
+    let icn = document.addEventListener("mouseover", (e) => {
+        e.target.setAttribute("class", "far fa-cog fa-spin");
+    });
+});
